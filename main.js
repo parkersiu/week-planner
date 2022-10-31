ae093ea496d3c7a0174900c8885d2b277ad5bf0f
Parker Siu <parker.siu@gmail.com>
2022-10-31 19:30:51 +0000

    funny and safe-for-work message

--- main.js
+++ main.js
@@ -13,20 +13,9 @@ console.log($form);
 $form.addEventListener('submit', function (event) {
 
   var entry = {};
-  entry.day = $form.elements[0].value;
+  entry.day = $form.elements['day-of-week'].value;
   entry.time = $form.elements.time.value;
   entry.description = $form.elements.description.value;
   entries.push(entry);
   $form.reset();
-});
-
-var entries = [];
-var $entries = localStorage.getItem('javascript storage');
-if ($entries !== null) {
-  entries = JSON.parse($entries);
-}
-
-window.addEventListener('beforeunload', function (event) {
-  var entriesJson = JSON.stringify(entries);
-  localStorage.setItem('javascript storage', entriesJson);
 });
