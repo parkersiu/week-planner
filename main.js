5b0299c733906f056ed8cd8cb3502317d9c8e9d3
Aysha Hussain <ayshafajar3@gmail.com>
2022-10-31 17:56:09 +0000

    funny and safe-for-work message

---
+++ main.js
@@ -0,0 +1,11 @@
+var $entry = document.querySelector('.entry-button');
+console.log($entry);
+
+var $container = document.querySelector('.container-two');
+console.log($container);
+$entry.addEventListener('click', function (event) {
+  $container.className = 'container-two modal visible';
+});
+
+var $submit = document.querySelector('submit-button');
+console.log($submit);
